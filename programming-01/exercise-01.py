08867fe287458cb3e29f47e8b700be400d066c75
Carlos Paiva <paivakk22@gmail.com>
2024-09-03 22:09:53 -0300

    some exercises done

---
+++ programming-01/exercise-01.py
@@ -0,0 +1,5 @@
+# create an algorithm that asks for the user's name via keyboard and prints this name on the screen
+
+name_request = input("Please, digit your name: ")
+
+print("Thanks, your name is ", name_request)
